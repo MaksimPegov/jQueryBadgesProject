97fff29b906a3d27a2de1d26990e442db262c260
MaksimPegov <maksim.peg@gmail.com>
2017-02-16 00:23:20 +0200

    Готово!!!

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,25 @@
 $(function() {
+  var params = {
+      url: 'https://www.codeschool.com/users/Maksim2005.json',
+      dataType: 'jsonp',
+      success: success
+  }
+  
+  $.ajax(params);
 
-  // your code will go here
 
+  function success(respons){
+    debugger
+    var badge;
+    var parent = $("#badges");
+    var finished = respons.courses.completed;
+    for(var i = 0; i < finished.length; i++){
+      badge =  $("<div class='course'>");
+      badge.append($("<h3>").text(finished[i].title));
+      badge.append($("<img src='" + finished[i].badge + "'>"));
+      badge.append($("<a href='" + finished[i].url + "' class ='btn btn-primary' target='_blank'>").text("See Course"))
+      parent.append(badge);
+      
+    }
+  }
 });
